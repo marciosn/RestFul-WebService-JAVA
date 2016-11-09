382609bbba56cfaf261efdf4cbf3d54df1af418d
Marcio Nobre <marcio.nobre@ifactory.com.br>
2016-11-09 15:48:27 -0300

    service funcionando

---
+++ src/br/com/server/limitless/RestFulController.java
@@ -0,0 +1,29 @@
+package br.com.server.limitless;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+@Path("/v1")
+public class RestFulController {
+	
+	@Path("{f}")
+	@GET
+	@Produces("application/json")
+	public Response convertFtoCfromInput(@PathParam("f") float f) throws JSONException {
+
+		JSONObject jsonObject = new JSONObject();
+		float celsius;
+		celsius = (f - 32) * 5 / 9;
+		jsonObject.put("F Value", f);
+		jsonObject.put("C Value", celsius);
+
+		String result = "@Produces(\"application/json\") Output: \n\nF to C Converter Output: \n\n" + jsonObject;
+		return Response.status(200).entity(result).build();
+	}
+}
